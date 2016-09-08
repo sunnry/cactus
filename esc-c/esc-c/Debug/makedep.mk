39dc40269e366b3c223859d5ccd8709d0d2a1eed
sunnry <sunhaihuan@gmail.com>
2016-09-08 21:09:39 +0800

    c language version of german ecs program, compiled ok on avr studio 6.2

---
+++ esc-c/esc-c/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+analog.c
+
+BLMC.c
+
+main.c
+
+PPM_Decode.c
+
+timer0.c
+
+twislave.c
+
