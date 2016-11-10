cbe2da111e127feb1295e934b4acd2d114276bd4
sunnry <sunhaihuan@gmail.com>
2016-11-10 15:04:59 +0800

    cactus-mega main file,forget to commit last time

---
+++ cactus-mega/cactus-mega.ino
@@ -0,0 +1,235 @@
+
+// If you want to use HardwareSerial only, and not have to include SoftwareSerial support, you 
+// can define SERIALCOMMAND_HARDWAREONLY in SerialCommand.h, which will cause it to build without
+// SoftwareSerial support.   This makes the library act as it used to before SoftwareSerial 
+// support was added, and you don't need this next include: 
+//#include <SoftwareSerial.h>  
+
+#include "SerialCommand.h"
+
+#include <Wire.h>
+#include "Kalman.h"
+
+#define RESTRICT_PITCH //comment out to restrict roll to +-90 deg instead
+
+#define arduinoLED 50   // Arduino LED on board
+
+SerialCommand SCmd;   // The demo SerialCommand object
+
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+/* IMU Data */
+double accX, accY, accZ;
+double gyroX, gyroY, gyroZ;
+int16_t tempRaw;
+
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+
+uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
+
+void setup()
+{  
+  pinMode(arduinoLED,OUTPUT);      // Configure the onboard LED for output
+  digitalWrite(arduinoLED,LOW);    // default to LED off
+
+  Serial1.begin(115200); 
+  Wire.begin();
+#if ARDUINO >= 157
+  Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+#else
+  TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+#endif
+
+  i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
+  i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
+  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+  while (i2cWrite(0x19, i2cData, 4, false)); // Write to all four registers at once
+  while (i2cWrite(0x6B, 0x01, true)); // PLL with X axis gyroscope reference and disable sleep mode
+
+  while (i2cRead(0x75, i2cData, 1));
+  if (i2cData[0] != 0x68) { // Read "WHO_AM_I" register
+    Serial.print(F("Error reading sensor"));
+    while (1);
+  }
+
+  delay(100); // Wait for sensor to stabilize
+
+  /* Set kalman and gyro starting angle */
+  while (i2cRead(0x3B, i2cData, 6));
+  accX = (i2cData[0] << 8) | i2cData[1];
+  accY = (i2cData[2] << 8) | i2cData[3];
+  accZ = (i2cData[4] << 8) | i2cData[5];
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  kalmanX.setAngle(roll); // Set starting angle
+  kalmanY.setAngle(pitch);
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  compAngleX = roll;
+  compAngleY = pitch;
+
+  timer = micros();
+  
+  // Setup callbacks for SerialCommand commands
+  //CMD0: balance self control.
+  //CMD1: user control
+  //CMD2: turn light on or off
+  //RES0: control board has ready
+  //RES1: receive command ok
+  //RES2: unknow command
+  SCmd.addCommand("CMD0",onSelfBalance);       // Turns LED on
+  SCmd.addCommand("CMD1",onUserControl);        // Turns LED off
+  SCmd.addCommand("CMD2",onLedControl);     // Echos the string argument back
+  SCmd.addCommand("P",process_command);  // Converts two arguments to integers and echos them back 
+  SCmd.addDefaultHandler(unrecognized);  // Handler for command that isn't matched  (says "What?") 
+  Serial1.println("RES0"); 
+}
+
+void loop()
+{
+  /* Update all the values */
+  while (i2cRead(0x3B, i2cData, 14));
+  accX = ((i2cData[0] << 8) | i2cData[1]);
+  accY = ((i2cData[2] << 8) | i2cData[3]);
+  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  tempRaw = (i2cData[6] << 8) | i2cData[7];
+  gyroX = (i2cData[8] << 8) | i2cData[9];
+  gyroY = (i2cData[10] << 8) | i2cData[11];
+  gyroZ = (i2cData[12] << 8) | i2cData[13];
+
+  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  double gyroXrate = gyroX / 131.0; // Convert to deg/s
+  double gyroYrate = gyroY / 131.0; // Convert to deg/s
+
+#ifdef RESTRICT_PITCH
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(roll);
+    compAngleX = roll;
+    kalAngleX = roll;
+    gyroXangle = roll;
+  } else
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleX) > 90)
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+#else
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+    kalmanY.setAngle(pitch);
+    compAngleY = pitch;
+    kalAngleY = pitch;
+    gyroYangle = pitch;
+  } else
+    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleY) > 90)
+    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+#endif
+
+  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * dt;
+  //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+  //gyroYangle += kalmanY.getRate() * dt;
+
+  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+
+  //get command from serial and process command
+  SCmd.readSerial();
+}
+
+void onSelfBalance()
+{
+  Serial1.println("LED on"); 
+  digitalWrite(arduinoLED,HIGH);  
+}
+
+void onUserControl()
+{
+  char *arg;  
+  arg = SCmd.next();    // Get the next argument from the SerialCommand object buffer
+  if (arg != NULL)      // As long as it existed, take it
+  {
+    Serial1.println(arg); 
+  }
+  Serial1.print("RES1:");Serial1.print(kalAngleX);Serial1.print(",");Serial1.println(kalAngleY);
+}
+
+void onLedControl()
+{
+  Serial1.println("LED off"); 
+  digitalWrite(arduinoLED,LOW);
+}
+
+void process_command()    
+{
+  int aNumber;  
+  char *arg; 
+
+  Serial.println("We're in process_command"); 
+  arg = SCmd.next(); 
+  if (arg != NULL) 
+  {
+    aNumber=atoi(arg);    // Converts a char string to an integer
+    Serial.print("First argument was: "); 
+    Serial.println(aNumber); 
+  } 
+  else {
+    Serial.println("No arguments"); 
+  }
+
+  arg = SCmd.next(); 
+  if (arg != NULL) 
+  {
+    aNumber=atol(arg); 
+    Serial.print("Second argument was: "); 
+    Serial.println(aNumber); 
+  } 
+  else {
+    Serial.println("No second argument"); 
+  }
+
+}
+
+// This gets set as the default handler, and gets called when no other command matches. 
+void unrecognized()
+{
+  Serial1.println("RES2"); //not known command 
+}
+
