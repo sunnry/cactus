3033d794fa85a0057aed337a81fb3c57a4129848
sunnry <sunhaihuan@gmail.com>
2016-12-22 20:26:06 +0800

    example how to control servo on P35 P36

---
+++ sample/servo/servo.ino
@@ -0,0 +1,27 @@
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(10);  // attaches the servo on pin 11 of arduino map, which is pin 24 of mega2560 P35 on  rov main control board 
+                       // you can attach on pin 10 of arduino map,which is pin 23 of mega2560 P36 on  rov main control board
+} 
+ 
+ 
+void loop() 
+{ 
+  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+} 
