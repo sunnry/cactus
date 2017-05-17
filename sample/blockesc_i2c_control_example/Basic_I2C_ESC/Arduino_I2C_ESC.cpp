8be871feaa742fca120a37b9b9ab783036bbeb05
sunnry <sunhaihuan@gmail.com>
2017-05-17 12:07:36 +0800

    根据ecs8a_v3的原理图修改的电流和温度计算
    测试ok，其中温度用查表法

--- sample/blockesc_i2c_control_example/Basic_I2C_ESC/Arduino_I2C_ESC.cpp
+++ sample/blockesc_i2c_control_example/Basic_I2C_ESC/Arduino_I2C_ESC.cpp
@@ -62,22 +62,33 @@ float Arduino_I2C_ESC::voltage() {
 }
 
 float Arduino_I2C_ESC::current() {
-  return (float(_current_raw)-32767)/65535.0f*5.0f*14.706f;
+  return float(_current_raw*100)/65536.0f*5.0f;
 }
 
 float Arduino_I2C_ESC::temperature() {
-  // This code was taken from an Adafruit
-	float resistance = SERIESRESISTOR/(65535/float(_temp_raw)-1);
+  int tempture =0;
+  //热敏电阻温度表，index表示温度，里面的值表示对应的电阻
+  float tempture_table[] = {27.8, 26.6, 25.46, 24.38, 23.35, 22.37, 21.44, 20.55, 19.70, 18.90, 18.13, 
+                                  17.39, 16.69, 16.02, 15.39, 14.78, 14.20, 13.64, 13.11, 12.61, 12.12,
+                                  11.66, 11.22, 10.79, 10.39, 10.00,  9.63,  9.27,  8.93,  8.61,  8.30,
+                                  8.00,  7.71,  7.43,  7.71,  6.92,   6.67,  6.44,  6.22,  6.00,  5.80,
+                                  5.60,  5.41,  5.23,  5.05,  4.88,   4.72,  4.57,  4.42,  4.27,  4.13,
+                                  4.00,  3.87,  3.75,  3.63,  3.51,   3.40,  3.30,  3.19,  3.09,  3.00,
+                                  2.91,  2.82,  2.73,  2.65,  2.57,   2.49,  2.42,  2.35,  2.28,  2.21,
+                                  2.15,  2.09,  2.03,  1.97,  1.91,   1.86,  1.80,  1.75,  1.70,  1.66,
+                                  1.61,  1.57,  1.52,  1.48,  1.44,   1.40,  1.37,  1.33,  1.29,  1.26,
+                                  1.23,  1.19,  1.16,  1.13,  1.10,   1.07,  1.05,  1.02,  0.99,  0.97};
 
-	float steinhart;
-	steinhart = resistance / THERMISTORNOMINAL;  // (R/Ro)
-	steinhart = log(steinhart);                  // ln(R/Ro)
-	steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-	steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-	steinhart = 1.0 / steinhart;                 // Invert
-	steinhart -= 273.15;                         // convert to C
+  float voltage = float(_temp_raw)/65536.0f*5.0f; 
+  float res = (10.0f*(5.0f - voltage))/voltage;
 
-	return steinhart;
+  for(int i=0;i<100;i++){
+    if(res > tempture_table[i]){
+      tempture = i;
+      break;
+    }
+  }
+  return tempture;
 }
 
 int16_t Arduino_I2C_ESC::rpm() {
